cbaca43d5dd637f090ebeddd6725e9956bf77e3e
mavey14 <43805242+mavey14@users.noreply.github.com>
2018-10-25 22:24:02 +0800

    MainMenu

---
+++ TempusState/Assets/GMScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMScript : MonoBehaviour {
+
+    public bool isgamepause;
+    public bool timestop;
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
