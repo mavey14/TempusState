6c7b940ddae69dc600389481fe2fd26af159259b
mavey14 <43805242+mavey14@users.noreply.github.com>
2018-10-27 20:10:04 +0800

    FacelessYoung

---
+++ TempusState/Assets/Script/CameraScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform lookAt;
+    private Transform camTransform;
+    private Camera cam;
+    private float distance = 20f;
+    private float currentX = 0f;
+    private float currentY = 0f;
+    //private float sensitivityX = 4.0f;
+    //private float sensitivityY = 1f;
+    private const float anglemin = -60f;
+    private const float anglemax = 60f;
+    private const float Maxdistance = 10f;
+    private const float MinDistance = 5f;
+	// Use this for initialization
+	void Start () {
+        camTransform = transform;
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y") * -1f;
+        currentY = Mathf.Clamp(currentY, anglemin, anglemax);
+
+        distance += Input.GetAxis("Mouse ScrollWheel") * 10f;
+        distance = Mathf.Clamp(distance, MinDistance, Maxdistance);
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+
+        camTransform.LookAt(lookAt.position);
+    }
+}
--- TempusState/Assets/Script/PlayerScript.cs
+++ TempusState/Assets/Script/PlayerScript.cs
@@ -4,13 +4,96 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    float zmov,speed;
+    Vector3 velo;
+    Rigidbody rb;
+    private bool move, turn;
+    Animator panim;
+
 	// Use this for initialization
 	void Start () {
-		
+        speed = 10f;
+        move = turn = true;
+        rb = GetComponent<Rigidbody>();
+        panim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        move = Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0;
+
+        GetMovement();
+
+        if (move)
+        {
+            pmove(zmov);
+        }
+        if (turn)
+        {
+            pturn();
+        }
+
+        panim.SetBool("move", move);
+        
 	}
+
+    void pturn()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y - 45, 0), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y + 45, 0), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y + 205, 0), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y - 205, 0), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y - 180, 0), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y - 90, 0), 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Camera.main.transform.eulerAngles.y + 90, 0), 0.1f);
+        }
+    }
+
+    void pmove(float zmov)
+    {
+        velo = transform.TransformDirection(new Vector3(0, 0, zmov > 0 ? zmov:-zmov)).normalized * speed;
+        rb.MovePosition(transform.position + velo * Time.fixedDeltaTime);
+    }
+
+    void GetMovement()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            zmov = Input.GetAxis("Vertical");
+        }
+        else if (Input.GetAxis("Horizontal") != 0)
+        {
+            zmov = Input.GetAxis("Horizontal");
+        }
+    }
 }
